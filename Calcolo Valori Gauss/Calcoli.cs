982e8f600287306bc4324419f70cc6cdc8f8bebf
riccardocannella <riccardo.cannella@studenti.unicam.it>
2017-05-10 18:43:30 +0200

    Progetto funzionante, piccolezze da correggere

---
+++ Calcolo Valori Gauss/Calcoli.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MathNet.Numerics.Integration;
+
+namespace Calcolo_Valori_Gauss
+{
+    class Calcoli
+    {
+        public static double CalcolaIntegrale(double a, double b, double sigma = 1, double mu = 0)
+        {
+            double piGreco = Math.PI;
+
+            double y = SimpsonRule.IntegrateComposite(x => 1 / Math.Sqrt(2 * piGreco * Math.Pow(sigma, 2)) * Math.Exp(-0.5 * Math.Pow((x - mu) / sigma, 2)), a, b, 100000);
+            return y;
+        }
+    }
+}
---
+++ Calcolo Valori Gauss/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace Calcolo_Valori_Gauss
+{
+    [Activity(Label = "Calcolo Valori Gauss", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            //<application android:label="Calcolo Valori Gauss" android:theme="@android:style/Theme.Material.Light.LightStatusBar"></application>
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Button BtnCalcola = FindViewById<Button>(Resource.Id.btnCalcola);
+
+            double risultato = double.NaN;
+            BtnCalcola.Click += (object sender, EventArgs e) =>
+            {
+                risultato = EseguiParsing();
+                TextView TxtRisultato = FindViewById<TextView>(Resource.Id.txtRisultato);
+                TxtRisultato.Text = risultato.ToString();
+            };
+        }
+
+        private double EseguiParsing()
+        {
+            EditText A = FindViewById<EditText>(Resource.Id.txtA);
+            double.TryParse(A.Text, out double a);
+
+            EditText B = FindViewById<EditText>(Resource.Id.txtB);
+            double.TryParse(B.Text, out double b);
+
+            EditText Mu = FindViewById<EditText>(Resource.Id.txtMu);
+            double.TryParse(Mu.Text, out double mu);
+
+            EditText Sigma = FindViewById<EditText>(Resource.Id.txtSigma);
+            double.TryParse(Sigma.Text, out double sigma);
+
+            return Calcoli.CalcolaIntegrale(a, b, sigma, mu);
+        }
+    }
+}
+
