45887419ff9c616b1e97cd70648ea5a197456652
riccardocannella <riccardo.cannella@studenti.unicam.it>
2017-05-11 12:27:28 +0200

    Cambiati i colori del grafico, finito

--- Calcolo Valori Gauss/GraphLayoutActivity.cs
+++ Calcolo Valori Gauss/GraphLayoutActivity.cs
@@ -42,6 +42,7 @@ namespace Calcolo_Valori_Gauss
         private PlotModel CreatePlotModel(double a = double.NaN, double b = double.NaN, double mu = 0.0, double sigma = 1.0)
         {
             var model = new PlotModel { Title = "Grafico della distribuzione" };
+<<<<<<< Updated upstream
 
             Func<double, double> fnGauss = (x) => 1 / Math.Sqrt(2 * Math.PI * Math.Pow(sigma, 2)) * Math.Exp(-0.5 * Math.Pow((x - mu) / sigma, 2));
 
@@ -49,6 +50,39 @@ namespace Calcolo_Valori_Gauss
             model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, MaximumPadding = 0.1, MinimumPadding = 0.1 });
             model.Axes.Add(new LinearAxis { Position = AxisPosition.Left, MaximumPadding = 0.1, MinimumPadding = 0.1 });
             model.Axes[1].AbsoluteMinimum = 0;
+=======
+            // questa funzione è la funzione di densità della distribuzione normale
+            Func<double, double> fnDensita = (x) => 1 / Math.Sqrt(2 * Math.PI * Math.Pow(sigma, 2)) * Math.Exp(-0.5 * Math.Pow((x - mu) / sigma, 2));
+            // creo la linea della funzione con dei limiti ampi a sufficienza
+            model.Series.Add(new FunctionSeries(fnDensita, (mu - 5 * sigma), (mu + 5 * sigma), 0.0001) { Color = OxyColors.OrangeRed, Background=OxyColor.FromRgb(220, 220, 220)});
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, MaximumPadding = 0.1, MinimumPadding = 0.1 });
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left, MaximumPadding = 0.1, MinimumPadding = 0.1, AbsoluteMinimum = 0 });
+            // linea per colorare la porzione di funzione delimitata da a e b
+            var area = new FunctionSeries(fnDensita, a, b, 0.0001);
+            // linea verticale per il punto a
+            var lineaA = new LineSeries();
+            lineaA.Points.Add(new DataPoint(a, 0));
+            lineaA.Points.Add(new DataPoint(a, fnDensita(a)));
+            // linea verticale per il punto b
+            var lineaB = new LineSeries();
+            lineaB.Points.Add(new DataPoint(b, 0));
+            lineaB.Points.Add(new DataPoint(b, fnDensita(b)));
+            var lineaAsse = new LineSeries();
+            //linea orizzontale da a a b
+            lineaAsse.Points.Add(new DataPoint(a, 0));
+            lineaAsse.Points.Add(new DataPoint(b, 0));
+            // setto i colori per l'area
+            area.Color = OxyColors.Black;
+            lineaA.Color = OxyColors.Black;
+            lineaB.Color = OxyColors.Black;
+            lineaAsse.Color = OxyColors.Black;
+            // aggiungo tutte le linee 
+            model.Series.Add(area);
+            model.Series.Add(lineaA);
+            model.Series.Add(lineaB);
+            model.Series.Add(lineaAsse);
+            // impongo al grafico di ridisegnarsi
+>>>>>>> Stashed changes
             model.InvalidatePlot(true);
             return model;
         }
